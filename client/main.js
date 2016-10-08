59bb8b019fc9780ed9f6ce226f0318266b54962e
Juraj Kubelka <juraj.kubelka@icloud.com>
2016-10-08 09:09:48 -0300

    Sending data to templates with helpers: using array

--- client/main.js
+++ client/main.js
@@ -1,6 +1,16 @@
-var image_data = {
-    img_src: "free.jpg",
-    img_alt: "Happy and Free"
-};
+var image_data = [
+    {
+        img_src: "free.jpg",
+        img_alt: "Happy and Free"
+    },
+    {
+        img_src: "consciousness.jpg",
+        img_alt: "Consciousness"
+    },
+    {
+        img_src: "infinity.jpg",
+        img_alt: "Infinity"
+    }
+];
 
-Template.images.helpers(image_data);
+Template.images.helpers({ images: image_data });
