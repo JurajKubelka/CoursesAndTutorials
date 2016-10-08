d328dbd33f62aef9abe2886f58aa7c731cffa17e
Juraj Kubelka <juraj.kubelka@icloud.com>
2016-10-08 13:22:59 -0300

    Fill server database with images.

--- server/main.js
+++ server/main.js
@@ -1,5 +1,23 @@
-import { Meteor } from 'meteor/meteor';
+import {
+    Meteor
+} from 'meteor/meteor';
 
-Meteor.startup(() => {
-  // code to run on server at startup
+Images = new Mongo.Collection("images");
+
+Meteor.startup(function () {
+    if (Images.find().count() == 0) {
+        Images.insert({
+            img_src: "free.jpg",
+            img_alt: "Happy and Free"
+        });
+        Images.insert({
+            img_src: "consciousness.jpg",
+            img_alt: "Consciousness"
+        });
+        Images.insert({
+            img_src: "infinity.jpg",
+            img_alt: "Infinity"
+        });
+    }
+    console.log("Number of images: " + Images.find().count());
 });
