1bba7196853a52724f01492ecf41a8bb389fbcc3
Juraj Kubelka <juraj.kubelka@icloud.com>
2016-10-08 16:35:50 -0300

    Implement image adding with a Bootstrap modal (part 1)

--- client/main.js
+++ client/main.js
@@ -1,8 +1,7 @@
 Images = new Mongo.Collection("images");
-console.log("Number of images: " + Images.find().count());
 
 Template.images.helpers({
-    images: Images.find({}, {sort: {rating: -1}})
+    images: Images.find({}, {sort: {createdOn: -1, rating: -1}})
     // first argument is filter (all images)
     // second argument is says we want revers sorted images by rating
 });
@@ -26,5 +25,18 @@ Template.images.events({
         var image_id = this.id; // image id
         Images.update({_id: image_id}, {$set: {rating: rating}});
         // first argument is filter, second argument is about what I want to change
+    }
+});
+
+Template.imageAddForm.events({
+    'submit .js-add-image': function (event) {
+        var img_src = event.target.img_src.value;
+        var img_alt = event.target.img_alt.value;
+        Images.insert({
+            img_src: img_src,
+            img_alt: img_alt,
+            createdOn: new Date(),
+        })
+        return false; // false prevents browser to refresh the page
     }
 });
