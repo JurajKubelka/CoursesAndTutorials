7c8c9d247623ffc97cbb4de3a6b0f6c395218531
Juraj Kubelka <juraj.kubelka@icloud.com>
2016-10-08 16:07:45 -0300

    Add an imagine rating function: Updating and sorting
    	> meteor add barbatus:stars-rating
    	> meteor update less

--- client/main.js
+++ client/main.js
@@ -2,7 +2,9 @@ Images = new Mongo.Collection("images");
 console.log("Number of images: " + Images.find().count());
 
 Template.images.helpers({
-    images: Images.find()
+    images: Images.find({}, {sort: {rating: -1}})
+    // first argument is filter (all images)
+    // second argument is says we want revers sorted images by rating
 });
 
 Template.images.events({
@@ -18,5 +20,11 @@ Template.images.events({
                 "_id": image_id
             }); // Mongo filter  
         })
+    },
+    'click .js-star-image': function (event) {
+        var rating = $(event.currentTarget).data("userrating");
+        var image_id = this.id; // image id
+        Images.update({_id: image_id}, {$set: {rating: rating}});
+        // first argument is filter, second argument is about what I want to change
     }
 });
