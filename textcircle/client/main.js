6ea2afd9ba0493a5c16a2e17343cb0d7a2c6dced
Juraj Kubelka <juraj.kubelka@icloud.com>
2016-10-14 23:08:31 -0300

    Using Session and Meteor.setInterval().

--- textcircle/client/main.js
+++ textcircle/client/main.js
@@ -2,12 +2,24 @@ this.Documents = new Mongo.Collection("documents");
 // if we write this.Documents, then Documents is accesible from outside.
 
 Template.editor.helpers({
-   docid: function() {
-       var doc = Documents.findOne();
-       if (doc) {
-           return doc._id;
-       } else {
-           return null;
-       }
-   }
+    docid: function () {
+        var doc = Documents.findOne();
+        if (doc) {
+            return doc._id;
+        } else {
+            return null;
+        }
+    }
+});
+
+
+Meteor.setInterval(function() {
+    Session.set("current_date", new Date());
+}, 1000);
+
+
+Template.date_display.helpers({
+    current_date: function () {
+        return Session.get("current_date");
+    }
 });
