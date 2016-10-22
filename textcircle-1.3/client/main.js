63c3d1e623086c922c6f74d6a6274829bae46d17
Juraj Kubelka <juraj.kubelka@icloud.com>
2016-10-22 11:13:28 -0300

    Source code from week 2 "User accounts, packages and methods".

--- textcircle-1.3/client/main.js
+++ textcircle-1.3/client/main.js
@@ -1,24 +1,70 @@
+// this collection stores all the documents 
+// if we write this.Documents, then Documents is accesible from outside the file.
 this.Documents = new Mongo.Collection("documents");
-// if we write this.Documents, then Documents is accesible from outside.
+// this collection stores sets of users that are editing documents
+EditingUsers = new Mongo.Collection("editingUsers");
+
 
 Template.editor.helpers({
+    // return the id of the first document you can find
     docid: function () {
         // we pass a shared document once it is available
         var doc = Documents.findOne();
         if (doc) {
             return doc._id;
         } else {
-            return null;
+            return undefined;
         }
     },
-
+    // configure the CodeMirror editor
     config: function () {
         return function (editor) {
             // here we get access to the ShareJS editor.
+            editor.setOption("lineNumbers", true);
+            editor.setOption("theme", "cobalt");
+            // set a callback that gets triggered whenever the user
+            // makes a change in the code editing window
             editor.on("change", function (cm_editor, info) {
-                // we inject new code to the iframe
-                $('#viewer_id').contents().find("html").html(cm_editor.getValue());
-            })
-        }
+                // send the current code over to the iframe for rendering
+                $("#viewer_id").contents().find("html").html(cm_editor.getValue());
+                // add editing user to the database collection
+                Meteor.call("addEditingUser");
+            });
+        };
     }
 });
+
+Template.editingUsers.helpers({
+    // retrieve a set of users that are editing this document
+    users: function () {
+        var doc, editingUsers, users;
+        doc = Documents.findOne();
+        if (!doc) {
+            return;
+        } // give up
+        editingUsers = EditingUsers.findOne({
+            docid: doc._id
+        });
+        if (!editingUsers) {
+            return;
+        } // give up
+        users = new Array();
+        var i = 0;
+        for (var user_id in editingUsers.users) {
+            users[i] = fixObjectKeys(editingUsers.users[user_id]);
+            i++;
+        }
+        return users;
+    }
+})
+
+// this renames object keys by removing hyphens to make the compatible 
+// with spacebars. 
+function fixObjectKeys(obj) {
+    var newObj = {};
+    for (key in obj) {
+        var key2 = key.replace("-", "");
+        newObj[key2] = obj[key];
+    }
+    return newObj;
+}
--- textcircle-1.3/server/main.js
+++ textcircle-1.3/server/main.js
@@ -1,11 +1,49 @@
-import { Meteor } from 'meteor/meteor';
+import {
+    Meteor
+} from 'meteor/meteor';
 
 this.Documents = new Mongo.Collection("documents");
-// if we write this.Documents, then Documents is accesible from outside.
+// if we write this.Documents, then Documents is accesible from outside the file.
+EditingUsers = new Mongo.Collection("editingUsers");
 
 Meteor.startup(() => {
-  if (! Documents.findOne()) {
-      // no documents yet
-      Documents.insert({title: "My New Document"});
-  }
+    if (!Documents.findOne()) {
+        // no documents yet
+        Documents.insert({
+            title: "My New Document"
+        });
+    }
 });
+
+// methods that provide write access to the data
+// we have to use it once we remove insecure package
+Meteor.methods({
+    // allows changes to the editing users collection 
+    addEditingUser: function () {
+        var doc, user, editingUsers;
+        doc = Documents.findOne();
+        if (!doc) {
+            return;
+        } // no doc give up
+        if (!this.userId) {
+            return;
+        } // no logged in user give up
+        // now I have a doc and possibly a user
+        user = Meteor.user().profile;
+        editingUsers = EditingUsers.findOne({
+            docid: doc._id
+        });
+        if (!editingUsers) { // no editing users have been stored yet
+            editingUsers = {
+                docid: doc._id,
+                users: {},
+            };
+        }
+        user.lastEdit = new Date();
+        editingUsers.users[this.userId] = user;
+        // upsert Mongo command: insert or update if filter matches
+        EditingUsers.upsert({
+            _id: editingUsers._id
+        }, editingUsers);
+    }
+})
